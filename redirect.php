91516120c789292706bef23c7dcc8de0aa184773
Samir Jani <114118419+samirjani03@users.noreply.github.com>
2025-06-06 05:24:01 +0530

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "url_shortener";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
